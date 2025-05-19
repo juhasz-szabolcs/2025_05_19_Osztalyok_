d7f54ecd2ac89a7efdd89f31213a82f7dfa9b33d
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-05-19 09:45:10 +0200

    learn metódus

--- student.py
+++ student.py
@@ -6,6 +6,9 @@ class Student:
     
     def introduce(self):
         print(f"Név: {self.name}, Kor: {self.age}, Pontszám: {self.score} ")
+        
+    def learn(self, points):
+        self.score += points
     
 tivadar = Student()
 # print(tivadar)
@@ -16,4 +19,6 @@ tivadar.sex = "male"
 tivadar.score = 20
 
 # print(f"Név: {tivadar.name}, Kor: {tivadar.age}, Pontszám: {tivadar.score} ")
+tivadar.introduce()
+tivadar.learn(12)
 tivadar.introduce()
