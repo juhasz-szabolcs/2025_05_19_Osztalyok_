72d0dd413519a6ea2da0a4d4b4dddbab142f0b24
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-05-19 09:39:45 +0200

    3. introduce függvény

--- student.py
+++ student.py
@@ -4,12 +4,16 @@ class Student:
     sex = ""
     score = 0
     
+    def introduce(self):
+        print(f"Név: {self.name}, Kor: {self.age}, Pontszám: {self.score} ")
+    
 tivadar = Student()
-print(tivadar)
+# print(tivadar)
 
 tivadar.name = "El Tivadar"
 tivadar.age = 16
 tivadar.sex = "male"
 tivadar.score = 20
 
-print(f"Név: {tivadar.name}, Kor: {tivadar.age}, Pontszám: {tivadar.score} ")
+# print(f"Név: {tivadar.name}, Kor: {tivadar.age}, Pontszám: {tivadar.score} ")
+tivadar.introduce()
